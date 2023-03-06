a12eb14789ac990d69664334dac4ea439a0a5865
Bijith <bijithkatteeri011@gmail.com>
2023-03-06 18:48:21 +0530

    completed

--- src/firebase/config.js
+++ src/firebase/config.js
@@ -1,19 +1,17 @@
-// Import the functions you need from the SDKs you need
-import firebase from 'firebase';
-import 'firebase/auth';
-import 'firebase/firestore'
-import 'firebase/storage'
+import firebase from "firebase";
+import "firebase/auth";
+import "firebase/firestore";
+import "firebase/storage";
 
 const firebaseConfig = {
-    apiKey: "AIzaSyD9tH7DDVXi5upWfQPgnqC3gl5A_3bYfY8",
-    authDomain: "olx-clone-2e90e.firebaseapp.com",
-    projectId: "olx-clone-2e90e",
-    storageBucket: "olx-clone-2e90e.appspot.com",
-    messagingSenderId: "992389452713",
-    appId: "1:992389452713:web:b00f831fc16bcf40826ab8",
-    measurementId: "G-N2FWDC2ZSP"
-  };
-  
+  apiKey: process.env.REACT_APP_API_KEY,
+  authDomain: process.env.REACT_APP_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_APP_ID,
+  measurementId: process.env.REACT_APP_MEASUREMENET_ID,
+};
 
 // Initialize Firebase
 
